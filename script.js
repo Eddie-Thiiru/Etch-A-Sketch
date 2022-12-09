c7792e26f78ae83aeff9d7a16711275fff2ce449
Eddie-Thiiru <eddiewachaga6@gmail.com>
2022-12-09 12:05:03 +0300

    Add hover effect to the grid box

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const container = document.querySelector("#container");
 const grid = document.createElement("div");
-grid.classList.add("columns");
+grid.classList.add("squares");
 
 const fragment = new DocumentFragment();
 for(let i = 0; i < 256; i++) {
     fragment.appendChild(grid.cloneNode(true));
 }
-
 container.appendChild(fragment);
+
+const div = document.querySelectorAll(".squares");
+
+div.forEach(div => {
+    div.addEventListener("mouseover", function (e) {
+        e.target.style.background = "blue"; 
+    });
+});
+
 
