76ffa202aaf5beb2165a31b9b8d6eca74e58e5ce
Eddie-Thiiru <eddiewachaga6@gmail.com>
2022-12-13 19:07:39 +0300

    Add a prompt button
    The button prompts user for the number of squares per side for a new grid,
    and then a function replaces the old grid with a new grid.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("#amountButton");
+
 const grid = document.createElement("div");
-grid.classList.add("squares");
+grid.classList.add("square");
 
 const fragment = new DocumentFragment();
 for(let i = 0; i < 256; i++) {
@@ -8,12 +10,35 @@ for(let i = 0; i < 256; i++) {
 }
 container.appendChild(fragment);
 
-const div = document.querySelectorAll(".squares");
+function getNumber () {
+    const answer = parseInt(prompt("Input the number of squares per side"));
 
-div.forEach(div => {
-    div.addEventListener("mouseover", function (e) {
-        e.target.style.background = "blue"; 
-    });
+    if (answer <= 100) {
+        console.log(answer * answer);
+        return answer * answer;
+    } 
+}
+
+function getSquares(e) {
+    const number = getNumber();
+    const list = document.getElementById("container");
+
+    while (list.hasChildNodes()) {
+        list.removeChild(list.firstChild);
+    }
+
+    const fragment = new DocumentFragment();
+    for(let i = 0; i < number; i++) {
+        fragment.appendChild(grid.cloneNode(true));
+    }
+    container.appendChild(fragment);
+}
+
+button.addEventListener("click", getSquares);
+
+// event delegation for dynamically created squares
+document.addEventListener("mouseover", function(e) {
+    if(e.target.className === "square") {
+        e.target.style.background = "blue";
+    }
 });
-
-
